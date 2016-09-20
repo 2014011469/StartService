e6ec6dbdfe9a8ed1dd40bdf0d58cf14b73ab13f2
unknown <>
2016-09-20 17:17:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/startservice/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.administrator.startservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnStart = (Button)findViewById(R.id.StartService);
+        Button btnEnd = (Button)findViewById(R.id.EndService);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                intent.putExtra("num",11);
+                startService(intent);
+            }
+        });
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
